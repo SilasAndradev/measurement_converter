f7adc72f3ae3c6a8947f960dcc23fcb4d8c157e6
SilasAndradev <silasandrade.code@gmail.com>
2025-01-20 11:24:23 -0300

    main

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -10,9 +10,28 @@ class APP:
 
         self.color_background = "#363f4e"
 
-        self.master.config(background = self.color_background)
 
-        
+        self.frame_master = Frame(
+            self.master, 
+            background = self.color_background,
+            width = 800,
+            height= 600
+            )
 
+        self.option = [
+            "Kilometer"
+            "Meter",
+            "Centimeter",
+            "Millimeter",
+            "Decimeter",
+        ]
+        self.MainWindow()
     def run(self):
         self.master.mainloop()
+
+    def MainWindow(self):
+        clicked = StringVar()
+        clicked.set(self.option[0])
+
+        drop = OptionMenu(self.frame_master, clicked, *self.option)
+        drop.pack()
