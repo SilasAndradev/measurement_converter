61b51871cb933b3ed8c63cf7a5d4f93dab0af39f
SilasAndradev <silasandrade.code@gmail.com>
2025-01-20 12:35:56 -0300

    main

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from measure.meters import meters
 
 class APP:
     def __init__(self):
@@ -9,17 +10,17 @@ class APP:
         self.master.title("Measurement Converter")
 
         self.color_background = "#363f4e"
-
+        self.color_background_text = "#87ceeb"
 
         self.frame_master = Frame(
             self.master, 
             background = self.color_background,
-            width = 800,
-            height= 600
-            )
+            width = self.master.winfo_screenwidth(),
+            height = self.master.winfo_screenheight(),
+            ).place(relx=0.5, rely=0.5, anchor=CENTER)
 
         self.option = [
-            "Kilometer"
+            "Kilometer",
             "Meter",
             "Centimeter",
             "Millimeter",
@@ -30,8 +31,27 @@ class APP:
         self.master.mainloop()
 
     def MainWindow(self):
-        clicked = StringVar()
-        clicked.set(self.option[0])
+        self.clicked_left = StringVar()
+        self.clicked_left.set(self.option[0])
 
-        drop = OptionMenu(self.frame_master, clicked, *self.option)
-        drop.pack()
+        self.clicked_right = StringVar()
+        self.clicked_right.set(self.option[0])
+
+        self.entry_left = Entry(self.frame_master, font=(0,10))
+        self.entry_left.place(relx=0.4, rely=0.5, anchor=CENTER, relheight=0.05)
+
+
+        self.dropdown_left = OptionMenu(self.frame_master, self.clicked_left, *self.option)
+        self.dropdown_right = OptionMenu(self.frame_master, self.clicked_right, *self.option)
+
+        self.dropdown_left.place(relx=0.4, rely=0.4, anchor=CENTER)
+        self.dropdown_right.place(relx=0.6, rely=0.4, anchor=CENTER)
+
+        self.convert_button = Button(self.frame_master, text='Convert', bg=self.color_background_text,
+        font=('Helvetica', 20, 'bold'), foreground='black', command=self.value_to_list).place(relx=0.5, rely=0.7, anchor=CENTER)
+
+    def value_to_list(self):
+        self.list_left = (self.clicked_left.get(), self.entry_left.get())
+        self.list_right = (self.clicked_right.get())
+
+        meters().main(self.list_left, self.list_right)
---
+++ src/measure/meters.py
@@ -0,0 +1,122 @@
+from tkinter import messagebox
+
+class meters:
+    def main(self, list_left, list_right):
+        self.type_left = list_left[0]
+        self.value_left = float(list_left[1])
+        
+        self.type_right = list_right
+        print(self.value_left)
+        print(self.type_right)
+
+        if self.type_left == "Kilometer":
+            self.Kilometer()
+
+        elif self.type_left == "Meter":
+            self.Meter()
+
+        elif self.type_left == "Centimeter":
+            self.Meter()
+
+        elif self.type_left == "Millimeter":
+            self.Millimeter()
+
+        elif self.type_left == "Decimeter":
+            self.Decimeter()
+
+
+
+    def Kilometer(self):
+        if self.type_right == "Kilometer":
+            messagebox.showinfo("Result", f"Result is {self.value_left}")
+
+        elif self.type_right == "Meter":
+            messagebox.showinfo("Result", f"Result is {self.value_left * 1000}")
+
+        elif self.type_right == "Centimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left * 1000 * 100}")
+
+        elif self.type_right == "Millimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left * 1000 * 1000}")
+
+        elif self.type_right == "Decimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left * 10 * 1000}")
+
+        else:
+            messagebox.showerror("ERROR", "Anything is error")
+
+    def Meter(self):
+        if self.type_right == "Kilometer":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 1000}")
+
+        elif self.type_right == "Meter":
+            messagebox.showinfo("Result", f"Result is {self.value_left}")
+
+        elif self.type_right == "Centimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 100}")
+
+        elif self.type_right == "Millimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 1000}")
+
+        elif self.type_right == "Decimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 10}")
+
+        else:
+            messagebox.showerror("ERROR", "Anything is error")
+
+    def Centimeter(self):
+        if self.type_right == "Kilometer":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 1000 / 100}")
+
+        elif self.type_right == "Meter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 100}")
+
+        elif self.type_right == "Centimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left}")
+
+        elif self.type_right == "Millimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 10}")
+
+        elif self.type_right == "Decimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left * 10}")
+
+        else:
+            messagebox.showerror("ERROR", "Anything is error")
+
+    def Millimeter(self):
+        if self.type_right == "Kilometer":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 1000000}")
+
+        elif self.type_right == "Meter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 1000}")
+
+        elif self.type_right == "Centimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 10}")
+
+        elif self.type_right == "Millimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left}")
+
+        elif self.type_right == "Decimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 100}")
+
+        else:
+            messagebox.showerror("ERROR", "Anything is error")
+
+    def Decimeter(self):
+        if self.type_right == "Kilometer":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 1000 / 10}")
+
+        elif self.type_right == "Meter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 10}")
+
+        elif self.type_right == "Centimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left / 10}")
+
+        elif self.type_right == "Millimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left * 100}")
+
+        elif self.type_right == "Decimeter":
+            messagebox.showinfo("Result", f"Result is {self.value_left}")
+
+        else:
+            messagebox.showerror("ERROR", "Anything is error")
